fe948f64641a30634bcbf9aa33b554fab29f3dc6
Fallutax <axelarmandoultimate@gmail.com>
2026-02-09 17:06:52 -0400

    arregle unos botones y modales, cambie un toast que estaba en alert y verifique que todo sea responsive

--- SGC_REACT/src/components/ButtonExp.jsx
+++ SGC_REACT/src/components/ButtonExp.jsx
@@ -83,7 +83,7 @@ export function ButtonExp() {
       <button
         onClick={handleExportExcel}
         disabled={loading}
-        className={`bg-gradient-to-r from-[#68bf74]/80 to-[#68bf74] hover:from-[#5aaf64] hover:to-[#5aaf64] text-white px-4 py-2 rounded-lg font-semibold transition-all duration-300 hover:brightness-110 shadow-lg flex items-center gap-2 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+        className={`flex-1 bg-gradient-to-r from-[#68bf74]/80 to-[#68bf74] hover:from-[#5aaf64] hover:to-[#5aaf64] text-white px-4 py-2 rounded-lg font-semibold transition-all duration-300 hover:brightness-110 shadow-lg flex items-center justify-center gap-2 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-file-earmark-excel" viewBox="0 0 16 16">
           <path d="M5.884 6.68a.5.5 0 1 0-.768.64L7.349 10l-2.233 2.68a.5.5 0 0 0 .768.64L8 10.781l2.116 2.54a.5.5 0 0 0 .768-.641L8.651 10l2.233-2.68a.5.5 0 0 0-.768-.64L8 9.219l-2.116-2.54z"/>
@@ -95,7 +95,7 @@ export function ButtonExp() {
       <button
         onClick={handleExportPDF}
         disabled={loading}
-        className={`bg-gradient-to-r from-[#3a3b3c]/70 to-[#3a3b3c]/70 hover:from-[#4a4b4c] hover:to-[#4a4b4c]/70 text-white px-4 py-2 rounded-lg font-semibold transition-all duration-300 hover:brightness-110 shadow-lg flex items-center gap-2 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
+        className={`flex-1 bg-gradient-to-r from-[#3a3b3c]/70 to-[#3a3b3c]/70 hover:from-[#4a4b4c] hover:to-[#4a4b4c]/70 text-white px-4 py-2 rounded-lg font-semibold transition-all duration-300 hover:brightness-110 shadow-lg flex items-center justify-center gap-2 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`}
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-file-earmark-pdf-fill" viewBox="0 0 16 16">
           <path d="M5.523 12.424q.21-.124.459-.238a8 8 0 0 1-.45.606c-.28.337-.498.516-.635.572l-.035.012a.3.3 0 0 1-.026-.044c-.056-.11-.054-.216.04-.36.106-.165.319-.354.647-.548m2.455-1.647q-.178.037-.356.078a21 21 0 0 0 .5-1.05 12 12 0 0 0 .51.858q-.326.048-.654.114m2.525.939a4 4 0 0 1-.435-.41q.344.007.612.054c.317.057.466.147.518.209a.1.1 0 0 1 .026.064.44.44 0 0 1-.06.2.3.3 0 0 1-.094.124.1.1 0 0 1-.069.015c-.09-.003-.258-.066-.498-.256M8.278 6.97c-.04.244-.108.524-.2.829a5 5 0 0 1-.089-.346c-.076-.353-.087-.63-.046-.822.038-.177.11-.248.196-.283a.5.5 0 0 1 .145-.04c.013.03.028.092.032.198q.008.183-.038.465z"/>
